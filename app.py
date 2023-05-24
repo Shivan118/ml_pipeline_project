7352c4bd75169428caefb45ec0f8d7241750917f
shivan <kshivan848@gmail.com>
2023-05-24 14:23:38 +0530

    flask application created

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
-from flask import Flask
-from src.logger import logging
-from src.exception import CustmeException
-import os, sys
+from flask import Flask, render_template, request, jsonify
+from src.pipeline.preediction_pipeline import PredictionPipeline, CustomeClass
+
 app = Flask(__name__)
 
+@app.route("/",methods = ["GET", "POST"])
+def prediction_data():
+    if request.method == "GET":
+        return render_template("home.html")
+    
+    else:
+        data = CustomeClass(
+            age = int(request.form.get("age")),
+            workclass = int(request.form.get("workclass")),
+            education_num = int(request.form.get("education_num")),
+            marital_status = int(request.form.get("marital_status")),
+            occupation = int(request.form.get("occupation")),
+            relationship = int(request.form.get("relationship")),
+            race = int(request.form.get("race")),
+            sex = int(request.form.get("sex")),
+            capital_gain = int(request.form.get("capital_gain")),
+            capital_loss = int(request.form.get("capital_loss")),
+            hours_per_week = int(request.form.get("hours_per_week")),
+            native_country = int(request.form.get("native_country")),
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
-    try:
-        raise Exception("we are testing our custom file")
-    except Exception as e:
-        abc = CustmeException(e, sys)
-        logging.info(abc.error_message)
-        return "Welcome to Engineering wala bhiya ML project Sesstion"
+        )
 
-if __name__ =="__main__":
-    app.run(debug=True)
+    final_data = data.get_data_DataFrame()
+    pipeline_prediction = PredictionPipeline()
+    pred = pipeline_prediction.predict(final_data)
+
+    result = pred
+
+    if result == 0:
+        return render_template("results.html", final_result = "Your Yearly Income is Less than Equal to 50k:{}".format(result) )
+
+    elif result == 1:
+            return render_template("results.html", final_result = "Your Yearly Income is More than 50k:{}".format(result) )
+    
+if __name__ == "__main__":
+     app.run(host = "0.0.0.0", debug = True)
+
+
+
+
+    
